c9283763649cda5e6666ea5ed9af650a0f030d69
MIRZA ZIYAD AHMED BAIG <beygz17@gmail.com>
2022-01-29 19:39:54 +0500

    server practice

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import express from "express"
+
+const app = express();
+
+// const PORT = process.env.PORT
+const PORT = '3000'
+
+
+app.get('/', function(req,res){
+    res.send(' EXPRESS 😎😎')
+})
+
+app.get('/tea', function(req, res){
+    res.send("HAVE TEA ☕")
+})
+
+app.get('/food', function(req, res){
+    res.send('HAVE FOOD 🍔')
+})
+
+app.get('/milk', function(req, res){
+    res.send('HAVE MILK 🥛')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`========== ISI SERVER RUNNING ON PORT ${PORT} ==========`)
+})
